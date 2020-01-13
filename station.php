966370be5ed354ca9331b2527629e4ebca3b9495
minikross <balta.olivier@gmail.com>
2020-01-13 16:53:45 +0100

    .

---
+++ station.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Titre de la page</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="script.js"></script>
+</head>
+<body>
+	
+
+	<?php 
+	/**********************************************************************
+	nom du script 	: station.php
+	description 	: Script qui se connecte au serveur de base de données 					et à la BD "Agence_voyage" pour interoger la table 					"station" et afficher les résultats dans un tablea HTML
+	auteur 			: M. BALTA
+	Version 		: 1.0
+	Date 			: 13/01/2020
+
+	**********************************************************************/
+
+
+
+	?>
+		<h1>Les stations </h1>
+	
+</body>
+</html>
